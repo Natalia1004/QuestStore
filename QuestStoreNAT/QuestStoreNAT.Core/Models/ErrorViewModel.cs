f593b5580f4e365b0fae2fd7d309538ce39d39f0
Tomasz Giela <chrono5@wp.pl>
2020-06-07 16:49:26 +0200

    Initial blank MVC Core project

---
+++ QuestStoreNAT/QuestStoreNAT.Core/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace QuestStoreNAT.Core.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
