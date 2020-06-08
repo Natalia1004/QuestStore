195b0c9cfa77b63ddea8d4a3e6d03a04c39727e4
Natalia Filipek <nataliafilipek@MacBook-Air-Natalia.local>
2020-06-08 23:12:02 +0200

    Add dbConnection and model student

---
+++ QuestStoreNAT/QuestStoreNAT.web/Models/Student.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace QuestStoreNAT.web.Models
+
+{
+    public class Student
+    {
+        public int StudentID { get; set; }
+
+        public int ClassID { get; set; }
+
+        public int GroupID { get; set; }
+
+        [Required(ErrorMessage = "Email Required")]
+        [EmailAddress(ErrorMessage = "Invalid email")]
+        public string Email { get; set; }
+
+        //add validaton data!
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Firstname Required")]
+        [StringLength(20, ErrorMessage = "2 to 20 characters.", MinimumLength = 2)]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Lastname Required")]
+        [StringLength(20, ErrorMessage = "2 to 20 characters.", MinimumLength = 2)]
+        public string LastName { get; set; }
+
+        public int CC_Wallet { get; set; }
+    }
+}
