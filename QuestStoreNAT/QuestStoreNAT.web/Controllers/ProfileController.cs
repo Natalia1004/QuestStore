ab213c6fdb9c62989345d08c31457fcf4eeceea7
Tomasz Giela <chrono5@wp.pl>
2020-06-09 18:29:27 +0200

    Add ProfileController

--- QuestStoreNAT/QuestStoreNAT.web/Controllers/ProfileController.cs
+++ QuestStoreNAT/QuestStoreNAT.web/Controllers/ProfileController.cs
@@ -3,12 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using QuestStoreNAT.web.Models;
 
 namespace QuestStoreNAT.web.Controllers
 {
     public class ProfileController : Controller
     {
-        public IActionResult Index()
+        private readonly Student testStudentModel;
+
+        public ProfileController()
+        {
+            testStudentModel = new Student();
+            testStudentModel.FirstName = "Robert";
+        }
+
+        public IActionResult Welcome()
+        {
+            //ten widok działa na modelu IUser
+            return View(testStudentModel);
+        }
+
+        public IActionResult MyProfile()
         {
             return View();
         }
