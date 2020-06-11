735534869a93fc3b93c60a427b8a69829381bfb6
Tomasz Giela <chrono5@wp.pl>
2020-06-11 17:22:41 +0200

    Add CredentialDAO

---
+++ QuestStoreNAT/QuestStoreNAT.web/DatabaseLayer/CredentialsDAO.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using QuestStoreNAT.web.Models;
+using System.Collections.Generic;
+using Npgsql;
+
+namespace QuestStoreNAT.web.DatabaseLayer
+{
+    public class CredentialsDAO
+    {
+        private const string dbTableName = "Credentials";
+        private enum CredentialsEnum
+        {
+            id, role, email, password
+        }
+
+        public Credentials FindCredentials(string email)
+        {
+            if (String.IsNullOrEmpty(email)) throw new ArgumentNullException(nameof(email));
+
+            using NpgsqlConnection connection = ConnectDB.CreateNewConnection();
+            var query = $"SELECT * FROM \"NATQuest\".\"{dbTableName}\" WHERE Email = '{email}' LIMIT 1;";
+            using var command = new NpgsqlCommand(query, connection);
+            var reader = command.ExecuteReader();
+
+            Credentials foundCredentials = null;
+            while (reader.Read())
+            {
+                foundCredentials = ProvideOneRecord(reader);
+            };
+            return foundCredentials;
+        }
+
+        private Credentials ProvideOneRecord(NpgsqlDataReader reader)
+        {
+            var credentials = new Credentials();
+            credentials.Id = reader.GetInt32((int)CredentialsEnum.id);
+            credentials.Role = (Role)reader.GetInt32((int)CredentialsEnum.role);
+            credentials.Email = reader.GetString((int)CredentialsEnum.email);
+            credentials.Password = reader.GetString((int)CredentialsEnum.password);
+            return credentials;
+        }
+    }
+}
