254a5a9301ffcf275dfb1ffe802e52e0dfda9baf
Tomasz Giela <chrono5@wp.pl>
2020-06-11 17:58:25 +0200

    Add ILoginValidatorService for dependency injection

---
+++ QuestStoreNAT/QuestStoreNAT.web/Services/ILoginValidatorService.cs
@@ -0,0 +1,9 @@
+﻿using QuestStoreNAT.web.Models;
+
+namespace QuestStoreNAT.web.Services
+{
+    public interface ILoginValidatorService
+    {
+        bool IsValidLogin(Credentials enteredCredentials);
+    }
+}
