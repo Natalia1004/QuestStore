4076530989eb4012849eb9d28c00f29d4661f5e0
Tomasz Giela <chrono5@wp.pl>
2020-06-11 17:23:39 +0200

    Add Session manager

---
+++ QuestStoreNAT/QuestStoreNAT.web/Services/ISession.cs
@@ -0,0 +1,14 @@
+﻿using QuestStoreNAT.web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuestStoreNAT.web.Services
+{
+    public interface ISession
+    {
+        public IUser LoggedUser { get; set; }
+        public Role LoggedUserRole { get; set; }
+    }
+}
---
+++ QuestStoreNAT/QuestStoreNAT.web/Services/Session.cs
@@ -0,0 +1,14 @@
+﻿using QuestStoreNAT.web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuestStoreNAT.web.Services
+{
+    public class Session : ISession
+    {
+        public IUser LoggedUser { get; set; }
+        public Role LoggedUserRole { get; set; }
+    }
+}
