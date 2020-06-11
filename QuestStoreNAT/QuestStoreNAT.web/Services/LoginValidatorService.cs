6348298805087d75479e8b0b8df9d6077e4cfd23
Tomasz Giela <chrono5@wp.pl>
2020-06-11 17:23:19 +0200

    Add LoginValidation service

---
+++ QuestStoreNAT/QuestStoreNAT.web/Services/LoginValidatorService.cs
@@ -0,0 +1,33 @@
+﻿using QuestStoreNAT.web.DatabaseLayer;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuestStoreNAT.web.Services
+{
+    public class LoginValidatorService
+    {
+        private readonly CredentialsDAO _CredentialsDAO; 
+
+        public LoginValidatorService()
+        {
+            _CredentialsDAO = new CredentialsDAO();
+        }
+
+        public bool VerifyLogin (string email, string password)
+        {
+            var fullCredentials = _CredentialsDAO.FindCredentials(email);
+
+            if (fullCredentials.Password == password)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
