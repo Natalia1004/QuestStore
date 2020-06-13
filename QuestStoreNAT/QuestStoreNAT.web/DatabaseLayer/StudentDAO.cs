487eb4525bbac3f645e87c10ab9d1600291a8ed5
Tomasz Giela <chrono5@wp.pl>
2020-06-13 12:30:15 +0200

    Add Student DAO

--- QuestStoreNAT/QuestStoreNAT.web/DatabaseLayer/StudentDAO.cs
+++ QuestStoreNAT/QuestStoreNAT.web/DatabaseLayer/StudentDAO.cs
@@ -1,11 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Npgsql;
+using QuestStoreNAT.web.Models;
 
 namespace QuestStoreNAT.web.DatabaseLayer
 {
-    public class StudentDAO
+    public class StudentDAO : DBAbstractRecord<Student>
     {
+        public override string DBTableName { get; set; } = "Students";
+        private enum StudentEnum
+        {
+            Id, ClassId, GroupId, Email, Password, FirstName, LastName, Wallet, Level
+        }
+
+        public override Student ProvideOneRecord(NpgsqlDataReader reader)
+        {
+            var student = new Student();
+            student.Id = reader.GetInt32((int)StudentEnum.Id);
+            student.FirstName = reader.GetString((int)StudentEnum.FirstName);
+            student.LastName = reader.GetString((int)StudentEnum.LastName);
+            student.Wallet = reader.GetInt32((int)StudentEnum.Wallet);
+            student.OverallWalletLevel = reader.GetInt32((int)StudentEnum.Level);
+            //TODO Credential retrieval ?
+            return student;
+        }
     }
 }
