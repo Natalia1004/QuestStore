b2bb0dc34127e678dda7fe511dfcf42d51d42e06
Tomasz Giela <chrono5@wp.pl>
2020-06-13 18:31:40 +0200

    Add QuestController

---
+++ QuestStoreNAT/QuestStoreNAT.web/Controllers/QuestController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using QuestStoreNAT.web.Services;
+
+namespace QuestStoreNAT.web.Controllers
+{
+    public class QuestController : Controller
+    {
+        private readonly ICurrentSession _session;
+
+        public QuestController(ICurrentSession session)
+        {
+            _session = session;
+        }
+
+        public IActionResult AddQuest()
+        {
+            ViewData["role"] = _session.LoggedUserRole;
+            return View();
+        }
+    }
+}
