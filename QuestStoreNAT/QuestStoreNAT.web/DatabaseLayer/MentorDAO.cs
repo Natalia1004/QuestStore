a0c389c2edfff477db8f0dec4377db627a6bccd6
Tomasz Giela <chrono5@wp.pl>
2020-06-13 12:29:37 +0200

    Add MentorDAO

--- QuestStoreNAT/QuestStoreNAT.web/DatabaseLayer/MentorDAO.cs
+++ QuestStoreNAT/QuestStoreNAT.web/DatabaseLayer/MentorDAO.cs
@@ -1,11 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Npgsql;
+using QuestStoreNAT.web.Models;
 
 namespace QuestStoreNAT.web.DatabaseLayer
 {
-    public class MentorDAO
+    public class MentorDAO : DBAbstractRecord<Mentor>
     {
+        public override string DBTableName { get; set; } = "Mentors";
+        private enum MentorsEnum
+        {
+            Id, FirstName, LastName, Bio
+        }
+
+        public override Mentor ProvideOneRecord(NpgsqlDataReader reader)
+        {
+            var mentor = new Mentor();
+            mentor.Id = reader.GetInt32((int)MentorsEnum.Id);
+            mentor.FirstName = reader.GetString((int)MentorsEnum.FirstName);
+            mentor.LastName = reader.GetString((int)MentorsEnum.LastName);
+            mentor.Bio = reader.GetString((int)MentorsEnum.Bio);
+            //TODO Credential retrieval ?
+            return mentor;
+        }
     }
 }
