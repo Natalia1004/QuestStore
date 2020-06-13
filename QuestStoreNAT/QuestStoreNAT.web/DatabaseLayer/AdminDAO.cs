447934142a6f8cda9b8735914a19f91d75c0359d
Tomasz Giela <chrono5@wp.pl>
2020-06-13 12:29:23 +0200

    Simplify AdminDAO

--- QuestStoreNAT/QuestStoreNAT.web/DatabaseLayer/AdminDAO.cs
+++ QuestStoreNAT/QuestStoreNAT.web/DatabaseLayer/AdminDAO.cs
@@ -1,44 +1,23 @@
 ﻿using Npgsql;
 using QuestStoreNAT.web.Models;
-using System;
 
 namespace QuestStoreNAT.web.DatabaseLayer
 {
-    public class AdminDAO
+    public class AdminDAO : DBAbstractRecord<Admin>
     {
-        private const string dbTableName = "Admins";
+        public override string DBTableName { get; set; } = "Admins";
         private enum AdminsEnum
         {
             Id, FirstName, LastName
         }
 
-        public Admin FindAdmin(string email)
+        public override Admin ProvideOneRecord(NpgsqlDataReader reader)
         {
-            if (String.IsNullOrEmpty(email)) throw new ArgumentNullException(nameof(email));
-
-            using NpgsqlConnection connection = ConnectDB.CreateNewConnection();
-            connection.Open();
-            var query = $"SELECT * FROM \"NATQuest\".\"{dbTableName}\" WHERE \"NATQuest\".\"{dbTableName}\".\"Email\" = '{email}' LIMIT 1;";
-            using var command = new NpgsqlCommand(query, connection);
-            var reader = command.ExecuteReader();
-
-            Admin foundAdmin = null;
-            while (reader.Read())
-            {
-                foundAdmin = ProvideOneRecord(reader, email);
-            };
-            return foundAdmin;
-        }
-
-        private Admin ProvideOneRecord(NpgsqlDataReader reader, string email)
-        {
-            var credentialsDAO = new CredentialsDAO();
-
             var admin = new Admin();
             admin.Id = reader.GetInt32((int)AdminsEnum.Id);
             admin.FirstName = reader.GetString((int)AdminsEnum.FirstName);
             admin.LastName = reader.GetString((int)AdminsEnum.LastName);
-            admin.Credentials = credentialsDAO.FindCredentials(email);
+            //TODO Credential retrieval ?
             return admin;
         }
     }
