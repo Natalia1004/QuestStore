0356fd6c14d6329c27fbaa32a1ccbb1ced50ee46
Tomasz Giela <chrono5@wp.pl>
2020-06-13 12:27:19 +0200

    Add explicitly DB connection strings

--- QuestStoreNAT/QuestStoreNAT.web/DatabaseLayer/ConnectDB.cs
+++ QuestStoreNAT/QuestStoreNAT.web/DatabaseLayer/ConnectDB.cs
@@ -1,10 +1,19 @@
-﻿using System;
-using Npgsql;
+﻿using Npgsql;
 
 namespace QuestStoreNAT.web.DatabaseLayer
 {
     public class ConnectDB
     {
+        private const string Host = "kandula.db.elephantsql.com";
+        private const string Username = "jvdwmero";
+        private const string Password = "GSy9rkphAxYyU__75_leDG1iIFQFQMVe";
+        private const string Database = "jvdwmero";
+
+        public static string GetConnectionString()
+        {
+            return $"Host={Host};Username={Username};Password={Password};Database={Database}";
+        }
+        
         public static void ExecuteNonQuery(string sql)
         {
             using NpgsqlConnection connection = CreateNewConnection();
@@ -16,7 +25,7 @@ namespace QuestStoreNAT.web.DatabaseLayer
 
         public static NpgsqlConnection CreateNewConnection()
         {
-            string accessConnection = "Host=kandula.db.elephantsql.com;Username=jvdwmero;Password=GSy9rkphAxYyU__75_leDG1iIFQFQMVe;Database=jvdwmero";
+            string accessConnection = GetConnectionString();
             NpgsqlConnection connection = new NpgsqlConnection(accessConnection);
             return connection;
         }
