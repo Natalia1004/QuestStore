a7d2a67ea3ee292479fc0264d34cd17aaf5b21ef
Tomasz Giela <chrono5@wp.pl>
2020-06-14 12:52:54 +0200

    Add handling AddQuest post requests in QuestController

--- QuestStoreNAT/QuestStoreNAT.web/Controllers/QuestController.cs
+++ QuestStoreNAT/QuestStoreNAT.web/Controllers/QuestController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using QuestStoreNAT.web.DatabaseLayer;
+using QuestStoreNAT.web.Models;
 using QuestStoreNAT.web.Services;
 
 namespace QuestStoreNAT.web.Controllers
@@ -16,8 +18,18 @@ namespace QuestStoreNAT.web.Controllers
             _session = session;
         }
 
+        [HttpGet]
         public IActionResult AddQuest()
         {
+            ViewData["role"] = _session.LoggedUserRole;
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddQuest(Quest questToAdd)
+        {
+            var questDAO = new QuestDAO();
+            questDAO.AddRecord(questToAdd);
+
             ViewData["role"] = _session.LoggedUserRole;
             return View();
         }
