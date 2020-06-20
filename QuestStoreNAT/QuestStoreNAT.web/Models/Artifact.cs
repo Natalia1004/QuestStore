b077e1a624c5ac2bdca03c0b3eb6cc74ce0d5d93
Tomasz Giela <chrono5@wp.pl>
2020-06-20 16:29:46 +0200

    Improve Artifact model by additional data anotations

--- QuestStoreNAT/QuestStoreNAT.web/Models/Artifact.cs
+++ QuestStoreNAT/QuestStoreNAT.web/Models/Artifact.cs
@@ -6,16 +6,22 @@ namespace QuestStoreNAT.web.Models
     {
         public int Id { get; set; }
 
-        [Required]
+        [Display(Name = "Type:")]
+        [Required(ErrorMessage = "Type required")]
         public TypeClassification Type { get; set; }
-        
-        [Required(ErrorMessage = "Artifact name required")]
+
+        [Display(Name = "Name:")]
+        [Required(ErrorMessage = "Name required")]
         [StringLength(20, ErrorMessage = "1 to 20 characters.", MinimumLength = 1)]
         public string Name { get; set; }
 
-        [Required]
+        [Display(Name = "Cost:")]
+        [Required(ErrorMessage = "Cost required")]
+        [Range(0, int.MaxValue, ErrorMessage = "Please enter valid integer")]
+        [RegularExpression(@"^[0-9]*$", ErrorMessage = "Please enter whole number")]
         public int Cost { get; set; }
 
+        [Display(Name = "Description:")]
         [Required(ErrorMessage = "Description required")]
         [StringLength(255, ErrorMessage = "1 to 255 characters.", MinimumLength = 1)]
         public string Description { get; set; }
