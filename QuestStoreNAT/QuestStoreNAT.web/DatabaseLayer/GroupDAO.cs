c211724440d6de16efd335b9b571b33847289c32
chashafe <chashafe@gmail.com>
2020-06-21 15:08:52 +0200

    initial

---
+++ QuestStoreNAT/QuestStoreNAT.web/DatabaseLayer/GroupDAO.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Npgsql;
+using QuestStoreNAT.web.Models;
+
+namespace QuestStoreNAT.web.DatabaseLayer
+{
+    public class GroupDAO : DBAbstractRecord<Group>
+    {
+        public override string DBTableName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public override Group ProvideOneRecord( NpgsqlDataReader reader )
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ProvideQueryStringToAdd( Group recordToAdd )
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ProvideQueryStringToUpdate( Group recordToUpdate )
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
