42b15d2d161c6d87c282ddce7a5d5d1845b45161
Tomasz Giela <chrono5@wp.pl>
2020-06-28 10:23:27 +0200

    Add IsValidPasswordHASH and SlowEquals method for comparing hashes

--- QuestStoreNAT/QuestStoreNAT.web/Services/LoginValidatorService.cs
+++ QuestStoreNAT/QuestStoreNAT.web/Services/LoginValidatorService.cs
@@ -1,4 +1,5 @@
-﻿using QuestStoreNAT.web.DatabaseLayer;
+﻿using LoginForm.Services;
+using QuestStoreNAT.web.DatabaseLayer;
 using QuestStoreNAT.web.Models;
 
 namespace QuestStoreNAT.web.Services
@@ -13,19 +14,52 @@ namespace QuestStoreNAT.web.Services
             _CredentialsDAO = new CredentialsDAO(); 
         }
 
-        public bool IsValidLogin (Credentials enteredCredentials)
-        {
-            Credentials validUserCredentils = _CredentialsDAO.FindCredentials(enteredCredentials.Email);
-
-            if (validUserCredentils is null) return false;
-
-            UserRole = validUserCredentils.Role;
-            return (validUserCredentils.Password == enteredCredentials.Password);
-        }
-
         public Role GetUserRole()
         {
             return UserRole;
+        }
+
+        public bool IsValidLogin (Credentials enteredCredentials)
+        {
+            Credentials userCredentialsInDb = _CredentialsDAO.FindCredentials(enteredCredentials.Email);
+
+            if (userCredentialsInDb is null) return false;
+
+            if (userCredentialsInDb.Password == enteredCredentials.Password)
+            {
+                UserRole = userCredentialsInDb.Role;
+                return true;
+            }
+            return false;
+        }
+
+        public bool IsValidPasswordHASH(Credentials enteredCredentials)
+        {
+            Credentials userCredentialsInDb = _CredentialsDAO.FindCredentials(enteredCredentials.Email);
+
+            if (userCredentialsInDb == null) return false;
+
+            var passwordFromDb = userCredentialsInDb.Password;
+            var saltFromDb = userCredentialsInDb.SALT;
+            var passwordFromForm = EncryptPassword.CreateHASH(enteredCredentials.Password, saltFromDb);
+
+            if (SlowEquals(passwordFromDb.ConvertStringToByte(), passwordFromForm))
+            {
+                UserRole = userCredentialsInDb.Role;
+                return true;
+            }
+            return false;
+        }
+
+        private bool SlowEquals(byte[] a, byte[] b)
+        {
+            //Byte comparison to prevent timing attacks
+            uint diff = (uint)a.Length ^ (uint)b.Length;
+            for (int i = 0; i < a.Length && i < b.Length; i++)
+            {
+                diff |= (uint)(a[i] ^ b[i]);
+            }
+            return diff == 0;
         }
     }
 }
