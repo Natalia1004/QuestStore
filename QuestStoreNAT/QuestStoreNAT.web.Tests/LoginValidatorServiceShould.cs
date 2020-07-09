46ef9d2f860afa8a470e2bcf41b512e51c07a678
Tomasz Giela <chrono5@wp.pl>
2020-07-09 12:09:44 +0200

    Add testing class for LoginValidatorService

---
+++ QuestStoreNAT/QuestStoreNAT.web.Tests/LoginValidatorServiceShould.cs
@@ -0,0 +1,31 @@
+﻿using Xunit;
+using Moq;
+using QuestStoreNAT.web.Services;
+using QuestStoreNAT.web.Models;
+using QuestStoreNAT.web.DatabaseLayer;
+
+namespace QuestStoreNAT.web.Tests
+{
+    public class LoginValidatorServiceShould
+    {
+        private LoginValidatorService sut;
+        private Mock<Credentials> mockCredentials;
+
+        public LoginValidatorServiceShould()
+        {
+            mockCredentials = new Mock<Credentials>();
+            mockCredentials.SetupAllProperties();
+            mockCredentials.Setup(x => x.Id).Returns(1);
+
+            var mockCredentialsDAO = new Mock<CredentialsDAO>();
+
+            sut = new LoginValidatorService(mockCredentialsDAO.Object);
+        }
+
+        [Fact]
+        public void ReturnTrue_When_PassedValidCredentials()
+        {
+
+        }
+    }
+}
