55581f2d28a49f883cd231a1975396ad8f01b43a
Tomasz Giela <chrono5@wp.pl>
2020-07-11 12:43:27 +0200

    Add tests to QuestControllerShould

--- QuestStoreNAT/QuestStoreNAT.web.Tests/QuestControllerShould.cs
+++ QuestStoreNAT/QuestStoreNAT.web.Tests/QuestControllerShould.cs
@@ -1,6 +1,9 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.Extensions.Logging;
 using Moq;
 using QuestStoreNAT.web.Controllers;
 using QuestStoreNAT.web.DatabaseLayer;
@@ -11,44 +14,48 @@ namespace QuestStoreNAT.web.Tests
 {
     public class QuestControllerShould
     {
-        private QuestController sutController;
-        private Mock<IDB_GenericInterface<Quest>> mockQuestDAO;
+        private readonly QuestController _sutController;
+        private readonly Mock<ILogger<QuestController>> _mockLogger;
+        private readonly Mock<IDB_GenericInterface<Quest>> _mockQuestDao;
+        private readonly Mock<HttpResponse> _mockHttpResponse;
 
         public QuestControllerShould()
         {
-            mockQuestDAO = new Mock<IDB_GenericInterface<Quest>>();
+            _mockLogger = new Mock<ILogger<QuestController>>();
+            _mockQuestDao = new Mock<IDB_GenericInterface<Quest>>();
 
-            var mockHttpResponse = new Mock<HttpResponse>();
-            mockHttpResponse.SetupAllProperties();
+            _mockHttpResponse = new Mock<HttpResponse>();
+            _mockHttpResponse.SetupAllProperties();
             var mockHttpContext = new Mock<HttpContext>();
             mockHttpContext.SetupAllProperties();
-            mockHttpContext.SetupGet(x => x.Response).Returns(mockHttpResponse.Object);
+            mockHttpContext.SetupGet(x => x.Response).Returns(_mockHttpResponse.Object);
 
             var mockTempData = new Mock<ITempDataDictionary>();
 
-            sutController = new QuestController(mockQuestDAO.Object);
-            sutController.TempData = mockTempData.Object;
-            sutController.ControllerContext = new ControllerContext();
-            sutController.ControllerContext.HttpContext = mockHttpContext.Object;
+            _sutController = new QuestController( _mockLogger.Object, _mockQuestDao.Object)
+            {
+                TempData = mockTempData.Object,
+                ControllerContext = new ControllerContext {HttpContext = mockHttpContext.Object}
+            };
         }
 
         [Fact]
-        public void ReturnDeleteView_When_PassedValidId()
+        public void ReturnsDeleteView_When_PassedValidIdTo_DeleteAction()
         {
-            mockQuestDAO.Setup(x => x.FindOneRecordBy(It.IsAny<int>())).Returns(new Quest() { Id = 1 });
+            _mockQuestDao.Setup(x => x.FindOneRecordBy(It.IsAny<int>())).Returns(new Quest() { Id = 1 });
              
-            var actionResult = sutController.DeleteQuest(1) as ViewResult;  
+            var actionResult = _sutController.DeleteQuest(1) as ViewResult;  
 
             Assert.NotNull(actionResult);
             Assert.Equal("DeleteQuest", actionResult.ViewName);
         }
 
         [Fact]
-        public void ReturnDeleteViewWithModel_When_PassedValidId()
+        public void ReturnDeleteViewWithModel_When_PassedValidIdTo_DeleteAction()
         {
-            mockQuestDAO.Setup(x => x.FindOneRecordBy(It.IsAny<int>())).Returns(new Quest() { Id = 1 });
+            _mockQuestDao.Setup(x => x.FindOneRecordBy(It.IsAny<int>())).Returns(new Quest() { Id = 1 });
 
-            var actionResult = sutController.DeleteQuest(1) as ViewResult;
+            var actionResult = _sutController.DeleteQuest(1) as ViewResult;
             var quest = actionResult.ViewData.Model;
 
             Assert.NotNull(actionResult);
@@ -56,25 +63,96 @@ namespace QuestStoreNAT.web.Tests
         }
 
         [Fact]
-        public void ReturnNotFoundView_When_ModelIsNull()
+        public void ReturnNotFoundView_When_ModelIsNullIn_DeleteAction()
         {
-            mockQuestDAO.Setup(x => x.FindOneRecordBy(It.IsAny<int>())).Returns((Quest)null);
+            _mockQuestDao.Setup(x => x.FindOneRecordBy(It.IsAny<int>())).Returns((Quest)null);
 
-            var actionResult = sutController.DeleteQuest(1) as ViewResult;
+            var actionResult = _sutController.DeleteQuest(1) as ViewResult;
 
             Assert.NotNull(actionResult);
+            Assert.Equal("NotFound", actionResult.ViewName);
+            _mockHttpResponse.VerifySet(x => x.StatusCode = 404);
+        }
+
+        [Fact]
+        public void SetStatusCodeTo404_When_ModelIsNullIn_DeleteAction()
+        {
+            _mockQuestDao.Setup(x => x.FindOneRecordBy(It.IsAny<int>())).Returns((Quest)null);
+
+            var actionResult = _sutController.DeleteQuest(1) as ViewResult;
+
+            _mockHttpResponse.VerifySet(x => x.StatusCode = 404);
+        }
+
+        [Fact]
+        public void RedirectToActionViewAllQuests_WhenValidQuestIsPassedTo_DeleteAction()
+        {
+            _mockQuestDao.Setup(x => x.FindOneRecordBy(It.IsAny<int>())).Returns(new Quest() { Id = 1 });
+
+            var actionResult = (RedirectToActionResult)_sutController.DeleteQuest(new Quest() { Id = 1 });
+
+            Assert.NotNull(actionResult);
+            Assert.Equal("ViewAllQuests", actionResult.ActionName);
+        }
+
+        [Fact]
+        public void ReturnNotFoundView_When_PassedNegativeIdTo_DeleteAction()
+        {
+            var actionResult = _sutController.DeleteQuest(-1) as ViewResult;
+
             Assert.Equal("NotFound", actionResult.ViewName);
         }
 
         [Fact]
-        public void RedirectToActionViewAllQuests_When_DeletingValidQuest()
+        public void ReturnErrorView_When_PassedNullQuestTo_DeleteAction()
         {
-            mockQuestDAO.Setup(x => x.FindOneRecordBy(It.IsAny<int>())).Returns(new Quest() { Id = 1 });
+            var actionResult = _sutController.DeleteQuest(null) as ViewResult;
 
-            var actionResult = (RedirectToActionResult)sutController.DeleteQuest(new Quest() { Id = 1 });
+            Assert.Equal("Error", actionResult.ViewName);
+        }
 
-            Assert.NotNull(actionResult);
-            Assert.Equal("ViewAllQuests", actionResult.ActionName);
+        [Fact]
+        public void ReturnViewAllQuestsView_When_SuccesfulDbRetrivalIn_ViewAllQuests()
+        {
+            _mockQuestDao.Setup(x => x.FetchAllRecords()).Returns(new List<Quest>());
+
+            var actionResult = _sutController.ViewAllQuests() as ViewResult;
+
+            Assert.Equal("ViewAllQuests", actionResult.ViewName);
+        }
+
+        [Fact]
+        public void ReturnErrorView_When_DbReturnsNoQuestsIn_ViewAllQuests()
+        {
+            _mockQuestDao.Setup(x => x.FetchAllRecords()).Returns((List<Quest>)null);
+
+            var actionResult = _sutController.ViewAllQuests() as ViewResult;
+
+            Assert.Equal("Error", actionResult.ViewName);
+        }
+
+        [Fact]
+        public void LogError_When_DbReturnsNoQuestsIn_ViewAllQuests()
+        {
+            _mockQuestDao.Setup(x => x.FetchAllRecords()).Returns((List<Quest>)null);
+
+            var actionResult = _sutController.ViewAllQuests();
+
+            _mockLogger.Verify(x => x.Log(
+                LogLevel.Error,
+                It.IsAny<EventId>(),
+                It.IsAny<It.IsAnyType>(),
+                It.IsAny<Exception>(),
+                (Func<It.IsAnyType, Exception, string>)It.IsAny<object>()), 
+                Times.Once);
+        }
+
+        [Fact]
+        public void ReturnAddView_When_Calls_AddQuests()
+        {
+            var actionResult = _sutController.AddQuest() as ViewResult;
+
+            Assert.Equal("AddQuest", actionResult.ViewName);
         }
     }
 }
