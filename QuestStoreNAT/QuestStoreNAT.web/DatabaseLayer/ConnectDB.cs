de8f6a5f3bfa5dae33ad7054716122dcf7813c17
Tomasz Giela <chrono5@wp.pl>
2020-07-11 19:31:33 +0200

    Update appsetting.json

--- QuestStoreNAT/QuestStoreNAT.web/DatabaseLayer/ConnectDB.cs
+++ QuestStoreNAT/QuestStoreNAT.web/DatabaseLayer/ConnectDB.cs
@@ -1,17 +1,26 @@
-﻿using Npgsql;
+﻿using Microsoft.Extensions.Configuration;
+using Npgsql;
 
 namespace QuestStoreNAT.web.DatabaseLayer
 {
     public class ConnectDB
     {
-        private const string Host = "kandula.db.elephantsql.com";
-        private const string Username = "jvdwmero";
-        private const string Password = "GSy9rkphAxYyU__75_leDG1iIFQFQMVe";
-        private const string Database = "jvdwmero";
+        private readonly IConfiguration _configuration;
+        //private const string Host = "kandula.db.elephantsql.com";
+        //private const string Username = "jvdwmero";
+        //private const string Password = "GSy9rkphAxYyU__75_leDG1iIFQFQMVe";
+        //private const string Database = "jvdwmero";
+
+        public ConnectDB(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
 
         public static string GetConnectionString()
         {
-            return $"Host={Host};Username={Username};Password={Password};Database={Database}";
+            var connectionString = _configuration.GetConnectionString("ElephantSQL");
+            return connectionString;
+            //return $"Host={Host};Username={Username};Password={Password};Database={Database}";
         }
         
         public static void ExecuteNonQuery(string sql)
