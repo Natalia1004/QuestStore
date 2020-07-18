9dc4d9a3932c9c9abbec11c951d5f9d617ce8059
chashafe <chashafe@gmail.com>
2020-07-18 12:32:08 +0200

    padding in show-all_box

--- QuestStoreNAT/QuestStoreNAT.web/Models/Classroom.cs
+++ QuestStoreNAT/QuestStoreNAT.web/Models/Classroom.cs
@@ -8,6 +8,7 @@ namespace QuestStoreNAT.web.Models
         public int Id { get; set; }
 
         [Required]
+        [StringLength(20, ErrorMessage = "2 to 16 characters.", MinimumLength = 2)]
         public string Name { get; set; }
 
         public List<Group> ClassroomGroups { get; set; }
